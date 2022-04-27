349f40ef2df27375f012bab9be70d48d3d40ef57
Vincenzo D'Aniello <vidaniello@gmail.com>
2022-04-27 17:48:28 +0200

    Develop progress

--- test/mongoTests.js
+++ test/mongoTests.js
@@ -1,10 +1,10 @@
 const { Collection, Db, MongoClient } = require('mongodb');
-
-
+const dotenv = require('dotenv');
+dotenv.config();
 
 var dbName = "testMongoFromNode";
 var collectionName = "customers";
-var url = `mongodb://172.16.16.65:27017/${dbName}`;
+var url = `mongodb://${process.env.HOSTNAME}:27017/${dbName}`;
 let d = '✅';
 
 /** @type {MongoClient} */
@@ -26,6 +26,7 @@ async function startTest(){
 
     try {
         //connection to DB
+        
         await dbClient.connect();
 
         db = dbClient.db();
@@ -45,11 +46,63 @@ async function startTest(){
         /** @type {Collection} */
         let custColl = db.collection(collectionName);
 
-        var myobj = { name: "Apple inc", address: "Cupertino" };
+        var myobj = { name: "Def", address: "Munich", street: "Fulp road"};
 
-        myobj = await custColl.insertOne(myobj);
+        let resInserted = await custColl.insertOne(myobj);
+
+        let id = resInserted.insertedId;
+
+        console.log(`the id of the new entity is: ${id}`)
+
+        
+
+        //Find
+        let query = { "_id": id};
+        
+        let resultFind = await custColl.find(query).toArray();
+        myobj = resultFind[0];
+
+        console.log(`the id of the entity: ${myobj._id}`);
+        console.log(`the name of the entity finded: ${myobj.name}`);
+
+
+        //Update
+        let newVal = {$set: {street: "Eram road"}};
+        let resUpdate = await custColl.updateOne(query, newVal);
+
+        console.log(`the result row modified: ${resUpdate.modifiedCount}`);
+        
+        resultFind = await custColl.find(query).toArray();
+        myobj = resultFind[0];
+
+        console.log(`the id of the entity: ${myobj._id}`);
+        console.log(`the streed modified: ${myobj.street}`);
+
+        //Replace
+        myobj.name = 'Imer';
+        delete myobj._id;
+        let resReplace = await custColl.replaceOne(query, myobj);
+        
+        console.log(`the replace result row are: ${resReplace.modifiedCount}`);
+
+        resultFind = await custColl.find(query).toArray();
+        myobj = resultFind[0];
+
+        console.log(`the name modified: ${myobj.name}`);
+        
+        //Delete
+        let resDelete = await custColl.deleteOne(query);
+
+        console.log(`the deleted row are: ${resDelete.deletedCount}`);
+
+        resultFind = await custColl.find(query).toArray();
+
+        console.log(`the result row find are: ${resultFind.length}`);
+        
+        let d = 0;
 
     } catch (err) {
+        console.error(err);
         throw err;
     } finally {
         process.exit();
